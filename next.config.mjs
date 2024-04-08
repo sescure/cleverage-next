252eef1c917e393f847152b9a22ac6dea475e1c3
Simon Escure <sescure@clever-age.com>
2024-04-08 15:25:11 +0200

    Add few packages

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// @ts-check
 
-export default nextConfig;
+/** @type {import('next').NextConfig} */ const path = require("path");
+const nextConfig = {
+  sassOptions: {
+    includePaths: [path.join(__dirname, "styles")],
+  },
+};
+
+module.exports = nextConfig;
